da691faa022dc2a0c7733838479115285f571ec3
ThayanaRMachado <thayanarm@hotmail.com>
2022-07-18 21:06:37 -0300

    Project created

---
+++ back-end/src/main/java/com/drinkwater/DrinkWaterApplication.java
@@ -0,0 +1,13 @@
+package com.drinkwater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkWaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkWaterApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/drinkwater/DrinkWaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkwater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkWaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
